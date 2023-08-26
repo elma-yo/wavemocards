ff640a69ed1a6ddc8997d6596fc38541cd263d65
Elma Yo <elmaspy@gmail.com>
2023-08-26 14:56:53 +0800

    Create findEmoBackground.html

---
+++ assets/js/textarea.js
@@ -0,0 +1,7 @@
+// 自適應高度的textarea
+var textarea = document.querySelector('textarea');
+        
+textarea.addEventListener('input', (e) => {
+    textarea.style.height = '100px';
+    textarea.style.height = e.target.scrollHeight + 'px';
+});
