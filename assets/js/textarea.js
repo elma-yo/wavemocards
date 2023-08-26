82168d9bfffe58bdc9a39f223ca9ca0a66259034
Elma Yo <elmaspy@gmail.com>
2023-08-26 19:00:36 +0800

    Create findEmoAction.html

--- assets/js/textarea.js
+++ assets/js/textarea.js
@@ -1,6 +1,5 @@
 // 自適應高度的textarea
 var textarea = document.querySelector('textarea');
-        
 textarea.addEventListener('input', (e) => {
     textarea.style.height = '100px';
     textarea.style.height = e.target.scrollHeight + 'px';
